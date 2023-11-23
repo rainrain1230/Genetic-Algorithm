1b745ac9d05c3ad15752b40cfda212db6943537a
rainrain1230 <chongtszwing@gmail.com>
2023-11-24 03:54:53 +0800

    Add type hinting

--- src/main.py
+++ src/main.py
@@ -1,46 +1,47 @@
-class Gene:
-    def __init__(self, segments):
-        self.segments = segments  # List of GeneSegments
-
+import hashlib
 class GeneSegment:
-    def __init__(self, identifier, quantity):
+    def __init__(self, identifier: str, quantity: float):
         self.identifier = identifier
         self.quantity = quantity
 
+class Gene:
+    def __init__(self, segments: list[GeneSegment]):
+        self.segments = segments
+
 class Protein:
-    def __init__(self, identifier, quantity):
+    def __init__(self, identifier: str, quantity: float):
         self.identifier = identifier
         self.quantity = quantity
 
 class Factor:
-    def __init__(self, identifier, quantity):
+    def __init__(self, identifier: str, quantity: float):
         self.identifier = identifier
         self.quantity = quantity
 
 class Neuron:
-    def __init__(self, input_hash, output_hash):
+    def __init__(self, input_hash: str, output_hash: str):
         self.input_hash = input_hash
         self.output_hash = output_hash
 
-def react(reactant, catalyst=None):
+def react(reactant: list[Protein|Factor], catalyst: list[Protein|Factor] = None):
     # Implement reaction logic here
     # For example, produce a product based on the reactant and catalyst
     pass
 
-def neuron_trigger(neuron, input_hash):
+def neuron_trigger(neuron: Neuron, input_hash: str):
     if neuron.input_hash == input_hash:
         return neuron.output_hash
     return None
 
-def mutate(gene):
+def mutate(gene: Gene):
     # Implement mutation logic
     pass
 
-def mate(gene1, gene2):
+def mate(gene1: Gene, gene2: Gene):
     # Implement mating logic
     pass
 
-def simulation_step(genes, proteins, factors, neurons):
+def simulation_step(genes: Gene, proteins: list[Protein], factors, neurons):
     # Implement one step of the simulation
     pass
 
@@ -50,5 +51,6 @@ proteins = [Protein(...)]
 factors = [Factor(...)]
 neurons = [Neuron(...)]
 
+number_of_steps = 100
 for step in range(number_of_steps):
     simulation_step(genes, proteins, factors, neurons)
